e1a02fc5b611119e69675edce16c580596b0303e
owo6289 <owo6289@gmail.com>
2023-11-09 23:53:10 +0800

    1109

--- bm.js
+++ bm.js
@@ -1,65 +1,73 @@
-function bracketMatch(inputString){
-     var stack = [];
-     var idx = 0;
-     var flag = true; //紀錄有無match
-     //var currentChar=inputString[0];//method 1
-     var currentChar=inputString.charAt(0); //method 2
-     //loop
-     while(currentChar != ''){
-        if(currentChar == '{' || currentChar=='[' || currentChar=='('){
-            stack.push(currentChar); //if是左括弧 丟到stack
-        }else if(currentChar == '}' || currentChar==']' || currentChar==')'){
-              if(stack.length==0){ //is stack empty?
-                  flag = false; //flag變成1 unmatch
-                  break;//unmatch
-              }else{
-                var openBracket = stack.pop();
-                if(   openBracket == '{' && currentChar=='}' 
-                   || openBracket == '[' && currentChar==']' 
-                   || openBracket == '(' && currentChar==')'){
-                     //match
-                   }
-                else{
-                    //unmatch
-                    flag = false; //flag變成1 unmatch
-                    break;
+function bracketMatch(inputString){ //input一字串,output是否正確
+    var stack = [];
+    var idx = 0;
+    var Bracket = {'{':'}', '[':']', '(':')', '<':'>'}; //儲存左右括弧的對應關係
+    var flag = true; //紀錄有無match
+    //var currentChar=inputString[0];//method 1
+    var currentChar=inputString.charAt(0); //method 2
+
+    //loop
+    while(currentChar != ''){
+    //if(currentChar == '{' || currentChar=='[' || currentChar=='(') //m1
+    if(Bracket[currentChar] != undefined){ //m2 if current有被bracket定義
+        stack.push(currentChar); //if是左括弧 丟到stack
+    }else if(currentChar == '}' || currentChar==']' || currentChar==')'){
+            if(stack.length==0){ //is stack empty?
+                flag = false; //flag變成1 unmatch
+                break;//unmatch
+            }else{
+            var openBracket = stack.pop(); //抓出字串最後一個元素跟current比
+            if(Bracket[openBracket] != currentChar){
+                flag = false;
+                break;
+            }
+            /*if(   openBracket == '{' && currentChar=='}' 
+                || openBracket == '[' && currentChar==']' 
+                || openBracket == '(' && currentChar==')'){
+                    //match
                 }
-              }
-        }
-        //next char
-        currentChar = inputString.charAt(++idx);
-     }
+            else{
+                //unmatch
+                flag = false; //flag變成1 unmatch
+                break;
+            }*/
+            } 
+    }
+    //next char
+    currentChar = inputString.charAt(++idx); //++index
+    }
 
-     if(stack.length==0 && flag){
-         //match
-         //console.log("Matched!")
-         return true;
-        
-     }else{
-         //unmatch
-         //console.log("Unmatched!")
-         return false;
-     }
-     
-     /*
-     if(stack.length==0 && idx == currentChar.length){ //next char = currentChar長度
-      //match
-      //console.log("Matched!")
-     
-      }else{
-      //unmatch
-      //console.log("Unmatched!")
-      }
+    //true/false?
+    if(stack.length==0 && flag){
+        //match
+        //console.log("Matched!")
+        return true;
+    
+    }else{
+        //unmatch
+        //console.log("Unmatched!")
+        return false;
+    }
+    
+    /*
+    if(stack.length==0 && idx == currentChar.length){ //next char = currentChar長度
+    //match
+    //console.log("Matched!")
+    
+    }else{
+    //unmatch
+    //console.log("Unmatched!")
+    }
 
-      if(stack.length==0 && currentChar == ' '){
-      //match
-      //console.log("Matched!")
-      
-      }else{
-      //unmatch
-      //console.log("Unmatched!")
-      }
-      */
+    if(stack.length==0 && currentChar == ' '){
+    //match
+    //console.log("Matched!")
+    
+    }else{
+    //unmatch
+    //console.log("Unmatched!")
+    }
+    */
 }
 
 
@@ -69,3 +77,4 @@ if(bracketMatch("([])}")){
 }else{
    console.log("Unmatched!")
 }
+
